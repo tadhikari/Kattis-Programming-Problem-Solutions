638b9d0c4b2440110de679dce341d1f999ae0041
Tribikram Adhikari <tribikram.adhikari@hotmail.com>
2018-01-06 10:29:06 -0600

    Add files via upload

---
+++ AAh/Ah.py
@@ -0,0 +1,10 @@
+def go_or_no(capacity,requirement):
+    if len(capacity) >= len(requirement):
+        return 'go'
+    else:
+        return 'no'
+
+cap = input()
+req = input()
+
+print(go_or_no(cap,req))
