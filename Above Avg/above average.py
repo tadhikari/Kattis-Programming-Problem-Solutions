d3bda69d9da896c18202c6f315ed50ac2c25518d
Tribikram Adhikari <tribikram.adhikari@hotmail.com>
2018-01-16 00:00:39 -0600

    programming problem

---
+++ Above Avg/above average.py
@@ -0,0 +1,23 @@
+def average(data):
+    total_num_data = int(data[0])
+    data = data[2:].split()
+    avg = 0
+    for i in data:
+        avg+= int(i)
+    avg = str(avg/total_num_data)
+    print(avg)
+    decimal  = addDecimal(avg.split('.')[1])
+    answer = "{}.{}%".format(avg.split('.')[0],decimal)
+    return answer
+        
+ 
+def addDecimal(decimal):
+    if len(decimal) < 3:
+        while len(decimal) < 3:
+            decimal+='0'
+    elif len(decimal) > 3:
+        decimal = decimal[:3]
+
+    return decimal
+
+print(average("5 50 50 70 80 100"))
