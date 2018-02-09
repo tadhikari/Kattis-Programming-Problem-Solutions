11bbde932d9c26490e3933bb6a2784f0623284b1
Tribikram Adhikari <tribikram.adhikari@hotmail.com>
2018-02-09 17:21:17 -0600

    Add files via upload

---
+++ Anthony and Diablo/anthonydiablo.py
@@ -0,0 +1,15 @@
+import math
+
+x = input()
+a,n = map(float,x.split())
+
+
+##a circle is the least area covering geometrical shape.
+
+radius = n/(math.pi*2)
+area = math.pi * radius**2
+
+if area >= a:
+    print('Diablo is happy!')
+else:
+    print('Need more materials!')
