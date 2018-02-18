33a7660e48c6875c2abd36756519ff5c0adb1481
Tribikram Adhikari <tribikram.adhikari@hotmail.com>
2018-02-17 19:24:31 -0600

    oddities

---
+++ oddities/oddities.py
@@ -0,0 +1,14 @@
+'''
+
+https://open.kattis.com/problems/oddities
+
+
+'''
+
+run = int(input())
+for i in range(run):
+    num = int(input())
+    if num%2 == 0:
+        print('{} is even'.format(num))
+    else:
+        print('{} is odd'.format(num))
