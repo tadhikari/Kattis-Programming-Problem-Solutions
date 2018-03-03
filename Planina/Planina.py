bc4ee92cb58e8a2c1ade7396d1c34d17c3a96bc9
Tribikram Adhikari <tribikram.adhikari@hotmail.com>
2018-03-03 00:45:21 -0600

    Difficulty: Easy (Tricky)

---
+++ Planina/Planina.py
@@ -0,0 +1,19 @@
+
+'''
+https://open.kattis.com/problems/planina
+
+'''
+
+initial_side = 2 #at initial state a side ocntains 2 points
+
+iteration = int(input())
+
+num_of_points = initial_side
+
+for i in range(iteration):
+    points = (initial_side + (initial_side - 1))
+    num_of_points = points**2
+    initial_side = points
+
+print(num_of_points)
+
