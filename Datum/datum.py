07ae0d749bbc6ad9c978a897b228d98c41eb062c
Tribikram Adhikari <tribikram.adhikari@hotmail.com>
2018-03-07 23:12:34 -0600

    Difficulty: Easy

--- Datum/datum.py
+++ Datum/datum.py
@@ -3,9 +3,6 @@ import datetime
 '''
 https://open.kattis.com/problems/datum
 '''
-
-
-
 days = ['Monday', 'Tuesday', 'Wednesday','Thursday','Friday', 'Saturday', 'Sunday']
 day,month = map(int,input().split())
 d = datetime.datetime(2009, month, day).weekday()
