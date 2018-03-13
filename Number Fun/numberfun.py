0e6427e1fe365801af53653d6ad8dde2a2a3918d
Tribikram Adhikari <tribikram.adhikari@hotmail.com>
2018-03-13 13:41:49 -0500

    Add files via upload

---
+++ Number Fun/numberfun.py
@@ -0,0 +1,13 @@
+'''
+https://open.kattis.com/problems/numberfun
+'''
+
+
+a = int(input())
+
+for i in range(a):
+    a,b,c = map(int,input().split())
+    if a+b == c or a/b == c or b/a == c or a-b==c or b-a==c or a*b==c:
+        print('Possible')
+    else:
+        print('Impossible')
